a4c8ea49c34ee17f513528420324bfda4fb664b9
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2021-07-11 02:17:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
-
+import random
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello oho!!!!!! yeah!!!!'
+    f=random.randint(0,9)
+    return f
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
