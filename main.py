eaeb3530360da11ba552467af62f4f05921b6b00
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2021-07-11 02:47:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
+import random
+
 
 from flask import Flask
-
+def get_bus_info(station):
+    info = []
+    for random_bus in range(10):
+        bus_name = f"W{random.randint(1, 100)}"
+        bus_arrival_time = random.randint(1, 30)
+        bus_info = f"{bus_name} 还有 {bus_arrival_time} 分钟到达 {station}"
+        info.append({bus_name: bus_info})
+    return info
 app = Flask(__name__)
-
+@app.route('/bus/<station>')
+def bus_info(station):
+    import bus
+    info = bus.get_bus_info(station)
+    return {
+        "msg": "success",
+        "data": info
+    }
 @app.route('/')
 def hello_world():
     return 'ok ok! that''s fine'
