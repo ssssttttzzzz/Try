8e2ca74bf786d29826910ed90051d6152dedf398
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2021-07-14 18:04:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def new_post():
     dict2.update(eval(json.dumps(post)))
     ta=int(dict2['a'])+int(dict2['b'])
     c={'jieguo':ta}
-    return dict2
+    return '<h1>yeah %s</h1>' %ta
  
 @app.route('/play') 
 def jsb():
