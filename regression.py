fc3f8200af7fa86f00206581e290ab45635622e7
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2021-07-25 02:50:58 +0800

    Update regression.py

--- regression.py
+++ regression.py
@@ -28,3 +28,17 @@ def ridgeRegres(xArr, yArr, lam=0.2):
         return
     ws = denom.I * (xMat.T * yMat)
     return ws
+def lwlr(testPoint, xArr, yArr, k=0.8):
+    xMat = mat(xArr);
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return ws
